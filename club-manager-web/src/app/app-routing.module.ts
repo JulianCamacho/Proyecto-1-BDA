22b2336d50503d65095f33c40599a9c19e9a9b5a
TheACE01 <keviinar51@gmail.com>
2022-09-10 13:16:49 -0600

    Student View and Login Component Template Added

--- club-manager-web/src/app/app-routing.module.ts
+++ club-manager-web/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Views/Student/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'login',
+     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
