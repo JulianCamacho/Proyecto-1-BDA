32858e4d011c72cd78c20580c2be346fe1e5351e
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2022-09-16 10:08:29 -0600

    Primera version backend

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+const { dbConnection } = require('./database/config')
+
+//crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+
+// Directorio publico
+app.use(express.static('public'));
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+
+//Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
